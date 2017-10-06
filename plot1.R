6c550aee2461996cf2fe9f4610f020091f941925
Nick Cotter <nick@extropy.net>
2017-10-06 19:58:44 +0100

    added trend line to plot 1

--- plot1.R
+++ plot1.R
@@ -1,16 +1,9 @@
 NEI <- readRDS("summarySCC_PM25.rds")
-#SCC <- readRDS("Source_Classification_Code.rds")
 
 total_by_year <- aggregate(NEI$Emissions, by=list(year=NEI$year), sum)
 
-
-plot(total_by_year$year, total_by_year$x/1000, xlab = "Year", ylab = "Total Emissions (Kilotons)", axes=FALSE)
-axis(1)
-pts <- pretty(total_by_year$x/1000)
-axis(2, at=pts, labels=pts)
-box()
+plot(total_by_year$year, total_by_year$x, xlab = "Year", ylab = "Total Emissions (Tons)", pch=16)
+abline(lm(total_by_year$x~total_by_year$year))
 
 # TODO
-# verify numbers
-# column instead of points
 # png output
