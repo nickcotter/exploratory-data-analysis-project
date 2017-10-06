b7edace91e24f74bc2c4f6da6d42925276d15422
Nick Cotter <nick@extropy.net>
2017-10-06 21:38:16 +0100

    basic plot 5

--- plot3.R
+++ plot3.R
@@ -4,7 +4,6 @@ NEI <- readRDS("summarySCC_PM25.rds")
 
 baltimore <- subset(NEI, fips == "24510")
 
-# column plot
 baltimore_total_by_year <- aggregate(baltimore$Emissions, by = list(year = baltimore$year), sum)
 ggplot(baltimore, aes(factor(year), Emissions)) + geom_col() + facet_wrap( ~ type, nrow=2, scales = 'free') + labs(x="Year", y = "Emissions (tons)")
 
---
+++ plot5.R
@@ -0,0 +1,15 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+vehicle_scc <- SCC[grep("vehicle", SCC$EI.Sector, ignore.case=TRUE),]
+
+baltimore <- subset(NEI, fips == "24510")
+baltimore_vehicle <- merge(baltimore, vehicle_scc, by="SCC")
+
+total_vehicle_by_year <- aggregate(baltimore_vehicle$Emissions, by=list(year=baltimore_vehicle$year), sum)
+
+barplot(total_vehicle_by_year$x, names.arg=total_vehicle_by_year$year, xlab="Year", ylab="Total Emissions (Tons)")
+
+# TODO
+# plot title
+# png output
